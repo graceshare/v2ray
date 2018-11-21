4dd07dd1b0a1758e3b58c376555fe24e889b9ec2
Vanilla <rose20.99.c@gmail.com>
2018-11-21 15:04:05 +0800

    [update]

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,15 @@
-#cd /v2raybin
-#echo -e "$CONFIG_JSON" > config.json
-#if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-#  echo -e "$CERT_PEM" > cert.pem
-#  echo -e "$KEY_PEM"  > key.pem
-#fi
+cd /v2raybin
 
-cd /v2ray
-wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
-unzip v2ray.zip 
-if [ ! -f "v2ray" ]; then
-  mv /v2ray/v2ray-v$VER-linux-64/v2ray .
-  mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
-  mv /v2ray/v2ray-v$VER-linux-64/geoip.dat .
-  mv /v2ray/v2ray-v$VER-linux-64/geosite.dat .
+# echo -e "$CONFIG_JSON" > config.json
+
+if [ "$CERT_PEM" != "$KEY_PEM" ]; then
+  echo -e "$CERT_PEM" > cert.pem
+  echo -e "$KEY_PEM"  > key.pem
 fi
 
 cp -f /config.json .
 chmod +x v2ray v2ctl
 sed -i "s/your_uuid/$UUID/g" config.json
 echo $UUID
+cat config.json
 ./v2ray
